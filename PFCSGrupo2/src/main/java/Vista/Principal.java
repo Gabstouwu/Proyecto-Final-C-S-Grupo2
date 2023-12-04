392253b91a64eca6d2d818f6c9d177278a38df0e
Gabriel G.M <ggutierrez00078@ufide.ac.cr>
2023-12-04 16:19:10 -0600

    a

--- PFCSGrupo2/src/main/java/Vista/Principal.java
+++ PFCSGrupo2/src/main/java/Vista/Principal.java
@@ -1,10 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
 package Vista;
 
 import Controlador.GestionClientes;
+import Controlador.GestionProductos;
 
 
 /**
